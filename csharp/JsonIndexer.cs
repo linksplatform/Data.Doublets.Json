6c09f4243f3449d79831b4af12fa02cf65c5844a
FreePhoenix <freephoenix888@gmail.com>
2021-05-27 16:52:31 +0600

    The csharp folder is added

---
+++ csharp/JsonIndexer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using Platform.Numbers;
+using Platform.Data.Numbers.Raw;
+using Platform.Data.Doublets;
+using Platform.Data.Doublets.Sequences.Frequencies.Cache;
+using Platform.Data.Doublets.Sequences.Frequencies.Counters;
+using Platform.Data.Doublets.Sequences.Indexes;
+using Platform.Data.Doublets.Unicode;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Data.Doublets.Json
+{
+    public class JsonIndexer<TLink> : IJsonStorage<TLink>
+    {
+        private static readonly TLink _zero = default;
+        private static readonly TLink _one = Arithmetic.Increment(_zero);
+
+        private readonly CachedFrequencyIncrementingSequenceIndex<TLink> _index;
+        private readonly CharToUnicodeSymbolConverter<TLink> _charToUnicodeSymbolConverter;
+        private TLink _unicodeSymbolMarker;
+        private readonly TLink _nullConstant;
+
+        public LinkFrequenciesCache<TLink> Cache { get; }
+
+        public JsonIndexer(ILinks<TLink> links)
+        {
+            _nullConstant = links.Constants.Null;
+            var totalSequenceSymbolFrequencyCounter = new TotalSequenceSymbolFrequencyCounter<TLink>(links);
+            Cache = new LinkFrequenciesCache<TLink>(links, totalSequenceSymbolFrequencyCounter);
+            _index = new CachedFrequencyIncrementingSequenceIndex<TLink>(Cache);
+            var addressToRawNumberConverter = new AddressToRawNumberConverter<TLink>();
+            InitConstants(links);
+            _charToUnicodeSymbolConverter = new CharToUnicodeSymbolConverter<TLink>(links, addressToRawNumberConverter, _unicodeSymbolMarker);
+        }
+
+        private void InitConstants(ILinks<TLink> links)
+        {
+            var markerIndex = _one;
+            var meaningRoot = links.GetOrCreate(markerIndex, markerIndex);
+            _unicodeSymbolMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(markerIndex));
+            _ = links.GetOrCreate(meaningRoot, Arithmetic.Increment(markerIndex));
+            _ = links.GetOrCreate(meaningRoot, Arithmetic.Increment(markerIndex));
+            _ = links.GetOrCreate(meaningRoot, Arithmetic.Increment(markerIndex));
+            _ = links.GetOrCreate(meaningRoot, Arithmetic.Increment(markerIndex));
+        }
+
+        public void AttachElementToParent(TLink elementToAttach, TLink parent)
+        {
+        }
+
+        public IList<TLink> ToElements(string @string)
+        {
+            var elements = new TLink[@string.Length];
+            for (int i = 0; i < @string.Length; i++)
+            {
+                elements[i] = _charToUnicodeSymbolConverter.Convert(@string[i]);
+            }
+            return elements;
+        }
+
+        public TLink CreateDocument(string name)
+        {
+            _index.Add(ToElements(name));
+            return _nullConstant;
+        }
+
+        public TLink CreateElement(string name)
+        {
+            _index.Add(ToElements(name));
+            return _nullConstant;
+        }
+
+        public TLink CreateTextElement(string content)
+        {
+            _index.Add(ToElements(content));
+            return _nullConstant;
+        }
+    }
+}
