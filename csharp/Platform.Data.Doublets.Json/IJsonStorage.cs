0dd7bd3f842f589b0d0a72a69b8899440927af7a
FreePhoenix <freephoenix888@gmail.com>
2021-05-29 20:28:13 +0600

    AttachObject is added

--- csharp/Platform.Data.Doublets.Json/IJsonStorage.cs
+++ csharp/Platform.Data.Doublets.Json/IJsonStorage.cs
@@ -10,6 +10,7 @@ namespace Platform.Data.Doublets.Json
         TLink CreateKey(TLink objectLink, string key);
         TLink CreateValue(TLink keyLink, string value);
         TLink CreateString(string content);
+        void AttachObject(TLink parent);
         void AttachElementToParent(TLink elementToAttach, TLink parent);
     }
 }
