56dc0efcedcf5c0a5e8922d9053147a515d848dd
FreePhoenix <freephoenix888@gmail.com>
2021-06-09 18:08:04 +0600

    Update AttachMemberToObject()

--- csharp/Platform.Data.Doublets.Json/DefaultJsonStorage.cs
+++ csharp/Platform.Data.Doublets.Json/DefaultJsonStorage.cs
@@ -151,7 +151,7 @@ namespace Platform.Data.Doublets.Json
         }
 
         public TLink AttachMemberToObject(string keyName, TLink @object) => _links.GetOrCreate(@object, CreateMember(keyName));
-        public TLink AttachMemberToObject(TLink keyLink, TLink @object) => _links.GetOrCreate(@object, keyLink);
+        public TLink AttachMemberToObject(TLink @object, TLink keyLink) => _links.GetOrCreate(@object, keyLink);
 
         public TLink Attach(TLink parent, TLink child) => _links.GetOrCreate(parent, child);
 
