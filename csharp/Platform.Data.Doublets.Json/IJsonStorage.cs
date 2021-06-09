7bacf514f25d8f53f6bf2420f7a48a282217918a
FreePhoenix <freephoenix888@gmail.com>
2021-06-09 17:38:46 +0600

    Update CreateMember()

--- csharp/Platform.Data.Doublets.Json/IJsonStorage.cs
+++ csharp/Platform.Data.Doublets.Json/IJsonStorage.cs
@@ -7,7 +7,7 @@ namespace Platform.Data.Doublets.Json
     public interface IJsonStorage<TLink>
     {
         TLink CreateDocument(string name);
-        TLink CreateMember(TLink objectLink, string key);
+        TLink CreateMember(string key);
         TLink CreateValue(TLink keyLink, string value);
         TLink CreateString(string content);
         TLink AttachObject(TLink parent);
