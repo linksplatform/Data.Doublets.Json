8e37f9a50ea9e8faed11a0bf2e00ed9d443e3cb9
FreePhoenix <freephoenix888@gmail.com>
2021-07-03 17:25:52 +0600

    Add draft

---
+++ csharp/Platform.Data.Doublets.Json/JsonImporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Threading;
+
+namespace Platform.Data.Doublets.Json
+{
+    class JsonImporter<TLink>
+    {
+        private readonly IJsonStorage<TLink> _storage;
+        private readonly TLink _document;
+        public JsonImporter(IJsonStorage<TLink> storage)
+        {
+            _storage = storage;
+            _document = _storage.CreateDocument("documentName");
+        }
+        public TLink Import(ref Utf8JsonReader utf8JsonReader, CancellationToken cancellationToken)
+        {
+
+            while (utf8JsonReader.Read())
+            {
+                switch (utf8JsonReader.TokenType)
+                {
+                    case JsonTokenType.String:
+                        _storage.AttachString(_document, utf8JsonReader.GetString());
+                        break;
+                }
+            }
+            return _document;
+        }
+    }
+}
