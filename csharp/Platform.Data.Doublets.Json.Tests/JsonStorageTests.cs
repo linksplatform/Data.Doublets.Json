d4de58ad2bf2c3e7b6dec62d4fb67f2e668c0658
FreePhoenix <freephoenix888@gmail.com>
2021-07-03 15:30:04 +0600

    Add test

--- csharp/Platform.Data.Doublets.Json.Tests/JsonStorageTests.cs
+++ csharp/Platform.Data.Doublets.Json.Tests/JsonStorageTests.cs
@@ -335,6 +335,20 @@ namespace Platform.Data.Doublets.Json.Tests
         }
 
         [Fact]
+        public void AttachFalseValueToKey()
+        {
+            ILinks<TLink> links = CreateLinks();
+            DefaultJsonStorage<TLink> defaultJsonStorage = new(links);
+            TLink document = defaultJsonStorage.CreateDocument("documentName");
+            TLink documentObjectValue = defaultJsonStorage.AttachObject(document);
+            TLink @object = defaultJsonStorage.GetObject(documentObjectValue);
+            TLink memberLink = defaultJsonStorage.AttachMemberToObject(@object, "keyName");
+            defaultJsonStorage.AttachBoolean(memberLink, false);
+            List<TLink> objectMembersLinks = defaultJsonStorage.GetMembersLinks(@object);
+            Assert.Equal(memberLink, objectMembersLinks[0]);
+        }
+
+        [Fact]
         public void GetObjectMembers()
         {
             /* EqualityComparer<TLink> equalityComparer = EqualityComparer<TLink>.Default;
