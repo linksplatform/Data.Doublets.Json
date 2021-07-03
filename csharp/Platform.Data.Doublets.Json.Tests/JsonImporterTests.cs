73e08c398c620d4850a83ed3d79573da2f498b69
FreePhoenix <freephoenix888@gmail.com>
2021-07-03 17:35:17 +0600

    Add test

---
+++ csharp/Platform.Data.Doublets.Json.Tests/JsonImporterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Platform.Data.Doublets.Json;
+
+namespace Platform.Data.Doublets.Json.Tests
+{
+    class JsonImporterTests
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            JsonImporter<Tlink> jsonImporter = new();
+        }
+    }
+}
