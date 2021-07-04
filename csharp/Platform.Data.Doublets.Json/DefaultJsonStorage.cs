0fe337fa1ff6055024c0327fef9cac39e46e0ee6
FreePhoenix <freephoenix888@gmail.com>
2021-07-04 22:19:14 +0600

    Add braces for if

--- csharp/Platform.Data.Doublets.Json/DefaultJsonStorage.cs
+++ csharp/Platform.Data.Doublets.Json/DefaultJsonStorage.cs
@@ -160,7 +160,11 @@ namespace Platform.Data.Doublets.Json
             for (int i = 0; i < 3; i++)
             {
                 TLink source = _links.GetSource(current);
-                if (equalityComparer.Equals(source, StringMarker)) return _links.GetTarget(current);
+                if (equalityComparer.Equals(source, StringMarker))
+                {
+                    return _links.GetTarget(current);
+                }
+
                 current = _links.GetTarget(current);
             }
             throw new Exception("The passed link does not contain string link.");
