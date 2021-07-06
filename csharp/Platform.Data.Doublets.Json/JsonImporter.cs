eff6f512b513fd98f03ef8e5ef4b311b5af62f89
FreePhoenix <freephoenix888@gmail.com>
2021-07-06 15:59:17 +0600

    Add property support

--- csharp/Platform.Data.Doublets.Json/JsonImporter.cs
+++ csharp/Platform.Data.Doublets.Json/JsonImporter.cs
@@ -19,39 +19,56 @@ namespace Platform.Data.Doublets.Json
             TLink document = _storage.CreateDocument(documentName);
             Stack<TLink> parents = new();
             parents.Push(document);
+            var isLastParentProperty = false;
             while (utf8JsonReader.Read())
             {
-                var parent = parents.Last();
-                switch (utf8JsonReader.TokenType)
+                var tokenType = utf8JsonReader.TokenType;
+                if (utf8JsonReader.TokenType == JsonTokenType.PropertyName)
                 {
-                    case JsonTokenType.StartObject:
-                        parents.Push(_storage.AttachObject(parent));
-                        break;
-                    case JsonTokenType.EndObject:
-                        parents.Pop();
-                        break;
-                    case JsonTokenType.String:
-                        _storage.AttachString(parent, utf8JsonReader.GetString());
-                        break;
-                    case JsonTokenType.Number:
-                        _storage.AttachNumber(parent, UncheckedConverter<int, TLink>.Default.Convert(utf8JsonReader.GetInt32()));
-                        break;
-                    case JsonTokenType.StartArray:
-                        var array = Array.Empty<TLink>();
-                        parents.Push(_storage.AttachArray(parent, array));
-                        break;
-                    case JsonTokenType.EndArray:
-                        parents.Pop();
-                        break;
-                    case JsonTokenType.True:
-                        _storage.AttachBoolean(parent, true);
-                        break;
-                    case JsonTokenType.False:
-                        _storage.AttachBoolean(parent, false);
-                        break;
-                    case JsonTokenType.Null:
-                        _storage.AttachNull(parent);
-                        break;
+                    parents.Push(_storage.AttachMemberToObject(parents.First(), utf8JsonReader.GetString()));
+                    isLastParentProperty = true;
+                }
+                if (tokenType == JsonTokenType.StartObject)
+                {
+                    parents.Push(_storage.AttachObject(parents.First()));
+                }
+                else if (tokenType == JsonTokenType.EndObject)
+                {
+                    parents.Pop();
+                }
+                else if (tokenType == JsonTokenType.String)
+                {
+                    _storage.AttachString(parents.First(), utf8JsonReader.GetString());
+                }
+                else if (tokenType == JsonTokenType.Number)
+                {
+                    _storage.AttachNumber(parents.First(), UncheckedConverter<int, TLink>.Default.Convert(utf8JsonReader.GetInt32()));
+                }
+                else if (tokenType == JsonTokenType.StartArray)
+                {
+                    var array = Array.Empty<TLink>();
+                    parents.Push(_storage.AttachArray(parents.First(), array));
+                }
+                else if (tokenType == JsonTokenType.EndArray)
+                {
+                    parents.Pop();
+                }
+                else if (tokenType == JsonTokenType.True)
+                {
+                    _storage.AttachBoolean(parents.First(), true);
+                }
+                else if (tokenType == JsonTokenType.False)
+                {
+                    _storage.AttachBoolean(parents.First(), false);
+                }
+                else if (tokenType == JsonTokenType.Null)
+                {
+                    _storage.AttachNull(parents.First());
+                }
+                if (isLastParentProperty && tokenType != JsonTokenType.PropertyName)
+                {
+                    isLastParentProperty = false;
+                    parents.Pop();
                 }
             }
 
