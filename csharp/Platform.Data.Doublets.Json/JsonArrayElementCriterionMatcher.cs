4f67f62156ad6db332c9dfc1c8069a48b3e9f0d1
FreePhoenix <freephoenix888@gmail.com>
2021-07-10 21:21:45 +0600

    Add classs that implements ICriterionMatcher

---
+++ csharp/Platform.Data.Doublets.Json/JsonArrayElementCriterionMatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Threading;
+using System.IO;
+using Platform.Converters;
+using System.Collections;
+using Platform.Data.Doublets.Sequences;
+using Platform.Data.Doublets.Sequences.HeightProviders;
+using Platform.Data.Doublets.Sequences.CriterionMatchers;
+using Platform.Interfaces;
+
+namespace Platform.Data.Doublets.Json
+{
+    class JsonArrayElementCriterionMatcher<TLink> : ICriterionMatcher<TLink>
+    {
+        public readonly IJsonStorage<TLink> Storage;
+        public JsonArrayElementCriterionMatcher(IJsonStorage<TLink> storage) => Storage = storage;
+        public bool IsMatched(TLink link) => EqualityComparer<TLink>.Default.Equals(Storage.Links.GetSource(link), Storage.ValueMarker);
+    }
+}
