3f6da0b6ed605e0f679e5b0f1cc590dc563b5d4e
FreePhoenix <freephoenix888@gmail.com>
2021-07-22 16:24:40 +0600

    Add console app

---
+++ csharp/Platform.Data.Doublets.Json.Exporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using TLink = System.UInt64;
+
+namespace Platform.Data.Doublets.Json.Exporter
+{
+    class Program
+    {
+        static void Main(params string[] args)
+        {
+            new JsonExporterCLI<TLink>().Run(args);
+        }
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Json.Importer/Program.cs
@@ -0,0 +1,13 @@
+﻿using Platform.Data.Doublets.Json;
+using TLink = System.UInt64;
+
+namespace JsonImporter
+{
+    class Program
+    {
+        static void Main(params string[] args)
+        {
+            new JsonImporterCLI<TLink>().Run(args);
+        }
+    }
+}
