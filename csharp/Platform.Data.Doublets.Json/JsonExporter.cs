824ca7c23851415bf08c34aae089b96be082a85e
FreePhoenix <freephoenix888@gmail.com>
2021-07-22 17:51:59 +0600

    Throw error if cancelled

--- csharp/Platform.Data.Doublets.Json/JsonExporter.cs
+++ csharp/Platform.Data.Doublets.Json/JsonExporter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
 using Platform.Converters;
@@ -171,6 +172,10 @@ namespace Platform.Data.Doublets.Json
             var valueLink = Storage.GetValueLink(documentLink);
             Write(ref utf8JsonWriter, valueLink, ref cancellationToken);
             utf8JsonWriter.Flush();
+            if (cancellationToken.IsCancellationRequested)
+            {
+                throw new Exception("Export aborted.");
+            }
         }
 
         public void Export(string documentName, Utf8JsonWriter utf8JsonWriter, CancellationToken cancellationToken) => Export(Storage.GetDocumentOrDefault(documentName), ref utf8JsonWriter, ref cancellationToken);
