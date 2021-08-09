d5ad6484b27fb31fc40f818761015c941dc79390
FreePhoenix <freephoenix888@gmail.com>
2021-08-09 23:17:58 +0600

    Optimise code

--- csharp/Platform.Data.Doublets.Json/JsonImporter.cs
+++ csharp/Platform.Data.Doublets.Json/JsonImporter.cs
@@ -8,16 +8,11 @@ namespace Platform.Data.Doublets.Json
     public class JsonImporter<TLink>
     {
         public readonly IJsonStorage<TLink> Storage;
-        public readonly EqualityComparer<TLink> EqualityComparer;
-        public readonly Stack<TLink> Parents;
-        public JsonImporter(IJsonStorage<TLink> storage)
-        {
-            Storage = storage;
-            EqualityComparer = EqualityComparer<TLink>.Default;
-            Parents = new ();
-        }
+        public readonly EqualityComparer<TLink> EqualityComparer = EqualityComparer<TLink>.Default;
+        public readonly Stack<TLink> Parents = new ();
+        public JsonImporter(IJsonStorage<TLink> storage) => Storage = storage;
 
-        private void PopIfParentIsMember()
+            private void PopIfParentIsMember()
         {
             var parent = Parents.Peek();
             var parentMarker = Storage.GetValueMarker(parent);
@@ -34,125 +29,140 @@ namespace Platform.Data.Doublets.Json
             {
                 throw new Exception("The document with the specified name already exists.");
             }
-            TLink document = Storage.CreateDocument(documentName);
+            var document = Storage.CreateDocument(documentName);
             Parents.Push(document);
+            TLink parent;
+            TLink parentMarker;
+            JsonTokenType tokenType;
+            TLink value;
+            TLink newParentArray;
             while (utf8JsonReader.Read())
             {
                 cancellationToken.ThrowIfCancellationRequested();
-                var parent = Parents.Peek();
-                var parentMarker = Storage.GetValueMarker(parent);
-                var tokenType = utf8JsonReader.TokenType;
+                parent = Parents.Peek();
+                parentMarker = Storage.GetValueMarker(parent);
+                tokenType = utf8JsonReader.TokenType;
                 if (utf8JsonReader.TokenType == JsonTokenType.PropertyName)
                 {
-                    Parents.Push(Storage.AttachMemberToObject(Storage.GetObject(parent), utf8JsonReader.GetString()));
+                    var @object = Storage.GetObject(parent);
+                    var property = utf8JsonReader.GetString();
+                    Parents.Push(Storage.AttachMemberToObject(@object, property));
                 }
-                if (tokenType == JsonTokenType.StartObject)
+                switch (tokenType)
                 {
-                    var value = Storage.CreateObjectValue();
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                    case JsonTokenType.StartObject:
                     {
+                        value = Storage.CreateObjectValue();
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, value);
+                            Parents.Push(newParentArray);
+                            Parents.Push(value);
+                        }
+                        else
+                        {
+                            var @object = Storage.Attach(parent, value);
+                            Parents.Push(@object);
+                        }
+                        break;
+                    }
+                    case JsonTokenType.EndObject:
                         Parents.Pop();
-                        var newArray = Storage.AppendArrayValue(parent, value);
-                        Parents.Push(newArray);
+                        break;
+                    case JsonTokenType.StartArray:
+                        value = Storage.CreateArrayValue(Array.Empty<TLink>());
                         Parents.Push(value);
-                    }
-                    else
+                        break;
+                    case JsonTokenType.EndArray:
                     {
-                        var @object = Storage.Attach(parent, value);
-                        Parents.Push(@object);
+                        var arrayValue = Parents.Pop();
+                        parent = Parents.Peek();
+                        parentMarker = Storage.GetValueMarker(parent);
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, arrayValue);
+                            Parents.Push(newParentArray);
+                        }
+                        Storage.Attach(parent, arrayValue);
+                        break;
                     }
-                }
-                else if (tokenType == JsonTokenType.EndObject)
-                {
-                    Parents.Pop(); 
-                }
-                else if (tokenType == JsonTokenType.StartArray)
-                {
-                    var value = Storage.CreateArrayValue(Array.Empty<TLink>());
-                    Parents.Push(value);
-                }
-                else if (tokenType == JsonTokenType.EndArray)
-                {
-                    var arrayValue = Parents.Pop();
-                    parent = Parents.Peek();
-                    parentMarker = Storage.GetValueMarker(parent);
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                    case JsonTokenType.String:
                     {
-                        var parentArray = Parents.Pop();
-                        var newParentArray = Storage.AppendArrayValue(parentArray, arrayValue);
-                        Parents.Push(newParentArray);
+                        var @string = utf8JsonReader.GetString();
+                        value = Storage.CreateStringValue(@string);
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, value);
+                            Parents.Push(newParentArray);
+                        }
+                        else
+                        {
+                            Storage.Attach(parent, value);
+                        }
+                        break;
                     }
-                    Storage.Attach(parent, arrayValue);
-                }
-                else if (tokenType == JsonTokenType.String)
-                {
-                    var @string = utf8JsonReader.GetString();
-                    var value = Storage.CreateStringValue(@string);
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                    case JsonTokenType.Number:
                     {
-                        Parents.Pop();
-                        var newArray = Storage.AppendArrayValue(parent, value);
-                        Parents.Push(newArray);
+                        value = Storage.CreateNumberValue(utf8JsonReader.GetDecimal());
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, value);
+                            Parents.Push(newParentArray);
+                        }
+                        else
+                        {
+                            Storage.Attach(parent, value);
+                        }
+                        break;
                     }
-                    else
+                    case JsonTokenType.True:
                     {
-                        Storage.Attach(parent, value);
+                        value = Storage.CreateBooleanValue(true);
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, value);
+                            Parents.Push(newParentArray);
+                        }
+                        else
+                        {
+                            Storage.Attach(parent, value);
+                        }
+                        break;
                     }
-                }
-                else if (tokenType == JsonTokenType.Number)
-                {
-                    var value = Storage.CreateNumberValue(utf8JsonReader.GetDecimal());
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                    case JsonTokenType.False:
                     {
-                        Parents.Pop();
-                        var newArray = Storage.AppendArrayValue(parent, value);
-                        Parents.Push(newArray);
+                        value = Storage.CreateBooleanValue(false);
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, value);
+                            Parents.Push(newParentArray);
+                        }
+                        else
+                        {
+                            Storage.Attach(parent, value);
+                        }
+                        break;
                     }
-                    else
+                    case JsonTokenType.Null:
                     {
-                        Storage.Attach(parent, value);
-                    }
-                }
-                else if (tokenType == JsonTokenType.True)
-                {
-                    var value = Storage.CreateBooleanValue(true);
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
-                    {
-                        Parents.Pop();
-                        var newArray = Storage.AppendArrayValue(parent, value);
-                        Parents.Push(newArray);
-                    }
-                    else
-                    {
-                        Storage.Attach(parent, value);
-                    }
-                }
-                else if (tokenType == JsonTokenType.False)
-                {
-                    var value = Storage.CreateBooleanValue(false);
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
-                    {
-                        Parents.Pop();
-                        var newArray = Storage.AppendArrayValue(parent, value);
-                        Parents.Push(newArray);
-                    }
-                    else
-                    {
-                        Storage.Attach(parent, value);
-                    }
-                }
-                else if (tokenType == JsonTokenType.Null)
-                {
-                    var value = Storage.CreateNullValue();
-                    if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
-                    {
-                        Parents.Pop();
-                        var newArray = Storage.AppendArrayValue(parent, value);
-                        Parents.Push(newArray);
-                    }
-                    else
-                    {
-                        Storage.Attach(parent, value);
+                        value = Storage.CreateNullValue();
+                        if (EqualityComparer.Equals(parentMarker, Storage.ArrayMarker))
+                        {
+                            Parents.Pop();
+                            newParentArray = Storage.AppendArrayValue(parent, value);
+                            Parents.Push(newParentArray);
+                        }
+                        else
+                        {
+                            Storage.Attach(parent, value);
+                        }
+                        break;
                     }
                 }
                 if (tokenType != JsonTokenType.PropertyName && tokenType != JsonTokenType.StartObject && tokenType != JsonTokenType.StartArray)
