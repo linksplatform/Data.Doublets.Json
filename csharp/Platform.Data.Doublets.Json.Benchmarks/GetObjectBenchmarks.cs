efd0ca9204a8e42383c732a426c1c566ca656b0c
FreePhoenix <freephoenix888@gmail.com>
2021-11-19 14:51:13 +0600

    Remove XML Documentation for tests and benchmarks

--- csharp/Platform.Data.Doublets.Json.Benchmarks/GetObjectBenchmarks.cs
+++ csharp/Platform.Data.Doublets.Json.Benchmarks/GetObjectBenchmarks.cs
@@ -12,12 +12,6 @@ using Platform.Data.Doublets.Sequences.Converters;
 
 namespace Platform.Data.Doublets.Json.Benchmarks
 {
-    /// <summary>
-    /// <para>
-    /// Represents the get object benchmarks.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     public class GetObjectBenchmarks
     {
         private ILinks<TLink> _links;
@@ -27,47 +21,13 @@ namespace Platform.Data.Doublets.Json.Benchmarks
         private TLink _documentObjectValueLink;
         private TLink _objectValueLink;
         private TLink _object;
-        /// <summary>
-        /// <para>
-        /// Creates the links.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>A links of t link</para>
-        /// <para></para>
-        /// </returns>
         public static ILinks<TLink> CreateLinks() => CreateLinks<TLink>(Path.GetTempFileName());
-        /// <summary>
-        /// <para>
-        /// Creates the links using the specified data db filename.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <typeparam name="TLink">
-        /// <para>The link.</para>
-        /// <para></para>
-        /// </typeparam>
-        /// <param name="dataDBFilename">
-        /// <para>The data db filename.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>A links of t link</para>
-        /// <para></para>
-        /// </returns>
         public static ILinks<TLink> CreateLinks<TLink>(string dataDBFilename)
         {
             var linksConstants = new LinksConstants<TLink>(enableExternalReferencesSupport: true);
             return new UnitedMemoryLinks<TLink>(new FileMappedResizableDirectMemory(dataDBFilename), UnitedMemoryLinks<TLink>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
         }
 
-        /// <summary>
-        /// <para>
-        /// Setup this instance.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [GlobalSetup]
         public void Setup()
         {
@@ -80,24 +40,6 @@ namespace Platform.Data.Doublets.Json.Benchmarks
             _object = _links.GetTarget(_objectValueLink);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the object wihout loop using the specified object value.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="objectValue">
-        /// <para>The object value.</para>
-        /// <para></para>
-        /// </param>
-        /// <exception cref="Exception">
-        /// <para>Not an object.</para>
-        /// <para></para>
-        /// </exception>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         public TLink GetObjectWihoutLoop(TLink objectValue)
         {
             EqualityComparer<TLink> equalityComparer = EqualityComparer<TLink>.Default;
@@ -116,96 +58,36 @@ namespace Platform.Data.Doublets.Json.Benchmarks
 
             throw new Exception("Not an object.");
         }
-        /// <summary>
-        /// <para>
-        /// Gets the object from document object value link benchmark.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public TLink GetObjectFromDocumentObjectValueLinkBenchmark()
         {
             return _defaultJsonStorage.GetObject(_documentObjectValueLink);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the object from object value link benchmark.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public TLink GetObjectFromObjectValueLinkBenchmark()
         {
             return _defaultJsonStorage.GetObject(_objectValueLink);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the object from object benchmark.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public TLink GetObjectFromObjectBenchmark()
         {
             return _defaultJsonStorage.GetObject(_object);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the object from document object value link without loop benchmark.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public TLink GetObjectFromDocumentObjectValueLinkWithoutLoopBenchmark()
         {
             return GetObjectWihoutLoop(_documentObjectValueLink);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the object from object value link without loop benchmark.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public TLink GetObjectFromObjectValueLinkWithoutLoopBenchmark()
         {
             return GetObjectWihoutLoop(_objectValueLink);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the object from object without loop benchmark.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The link</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public TLink GetObjectFromObjectWithoutLoopBenchmark()
         {
--- csharp/Platform.Data.Doublets.Json.Benchmarks/Program.cs
+++ csharp/Platform.Data.Doublets.Json.Benchmarks/Program.cs
@@ -4,20 +4,8 @@ using System;
 
 namespace Platform.Numbers.Benchmarks
 {
-    /// <summary>
-    /// <para>
-    /// Represents the program.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     static class Program
     {
-        /// <summary>
-        /// <para>
-        /// Main.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         static void Main()
         {
             BenchmarkRunner.Run<GetObjectBenchmarks>();
