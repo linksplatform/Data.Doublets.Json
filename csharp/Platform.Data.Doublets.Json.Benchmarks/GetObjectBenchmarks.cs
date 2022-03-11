e93ca3e0c838c0dc2bb7da93eabab126e47392fe
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-03-11 17:31:54 +0600

    Update GetObjectBenchmarks.cs

--- csharp/Platform.Data.Doublets.Json.Benchmarks/GetObjectBenchmarks.cs
+++ csharp/Platform.Data.Doublets.Json.Benchmarks/GetObjectBenchmarks.cs
@@ -1,4 +1,4 @@
-using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes; 
 using Platform.Data.Doublets.Memory.United.Generic;
 using Platform.Data.Doublets.Memory;
 using Platform.Memory;
